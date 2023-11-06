0ebc22e47f9ef0c2b1146f28b015aea6dd7a9457
Gillvano <freegilioshouse@gmail.com>
2023-11-06 11:13:14 +0100

    BattleSkills ASP.NET made

---
+++ BattleSkills/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BattleSkills.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
